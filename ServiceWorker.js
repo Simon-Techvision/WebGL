349c64509a45d2d8974103aab09f2f2b0d79b766
simon <abugulivinusbro@gmail.com>
2025-06-03 13:09:21 +0100

    w

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-WHAnalytics-0.1";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/80fb5d124be755833bee835124d5fa9d.framework.js",
-    "Build/c8037e7e52171a9a6102ce2744a3445b.data",
-    "Build/42e4ef7b28e9b11236b78072a32b0b6a.wasm",
+    "Build/6e9965c6c3783e6b9c2a160d7110d540.framework.js",
+    "Build/45288775ad55cac616a1db1f38cdb507.data",
+    "Build/25aef86d20dae20a2261d654f343667a.wasm",
     "TemplateData/style.css"
 
 ];
